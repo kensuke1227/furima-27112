cfbe14993dfbf3ad55ccf2f9fac7b368f1e948c3
kensuke1227 <kensuke.yamanaka@di-v.co.jp>
2020-12-12 15:34:05 +0900

    ⑦フォームオブジェクトを使用して保存できるようにしましょう

--- app/forms/pay_form.rb
+++ app/forms/pay_form.rb
@@ -5,4 +5,19 @@ class PayForm
  attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
   # フォームオブジェクトで使用する値を定義している（読み書きができるようになっている）
 
+  def save
+    order = Order.create(
+      item_id: item_id,
+      user_id: user_id
+    )
+    Address.create(
+      order_id: order.id,
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      addresses: addresses,
+      building: building,
+      phone_number: phone_number
+    )
+  end
 end
