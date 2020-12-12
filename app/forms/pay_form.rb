d504f62cdb91e0ddc0c611445c92799e15b9a9a6
kensuke1227 <kensuke.yamanaka@di-v.co.jp>
2020-12-12 15:38:32 +0900

    pay_form.rb完成

--- app/forms/pay_form.rb
+++ app/forms/pay_form.rb
@@ -5,6 +5,19 @@ class PayForm
  attr_accessor :item_id, :token, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id
   # フォームオブジェクトで使用する値を定義している（読み書きができるようになっている）
 
+# <<バリデーション>>
+  with_options presence: true do
+    validates :item_id
+    validates :token, presence: { message: "can't be blank" }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :city
+    validates :addresses
+    validates :phone_number, length: { maximum: 11, message: 'Too long' }
+    # 電話番号は入力フォームで制限してますが、一応つけています。
+    validates :user_id
+  end
+
   def save
     order = Order.create(
       item_id: item_id,
