34a1f9634321d6debbc0b62afe769eeb7a105a42
kensuke1227 <kensuke.yamanaka@di-v.co.jp>
2020-12-12 14:38:48 +0900

    123フォームオブジェクトのインスタンス変数を生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :select_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
+
   def index
+    return redirect_to root_path if current_user.id == select_item.user_id || select_item.order!=nil
+
+    @item_orders = PayForm.new
   end
  
   def create
   end
+
+  private
+
+  def select_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
